10c103360e6733d0a160706f4cbf10d7c7d001d7
Velpre <veljko_kv@hotmail.com>
2021-06-09 14:46:30 +0200

    starter med home page

--- JS/home.js
+++ JS/home.js
@@ -1,180 +1,148 @@
-const overlay = document.querySelector(".overlay");
-const message3 = document.querySelector(".msg3_time");
-const msg3 = document.querySelector(".msg3");
-const inboxName = document.querySelector(".inbox_name");
-const messageTxt = document.querySelectorAll(".message_txt");
-const expandable = document.querySelectorAll(".expandable");
-const yesterdayTxt = document.querySelectorAll(".yesterday");
-const todayTimeTxt = document.querySelector(".timeToday");
-const todayDateTxt = document.querySelector(".dateToday");
-const time = document.querySelectorAll(".time");
+// Printer ut home content
+const mainContentHome = document.querySelector(".main-content");
+const navbarHome = document.querySelector(".navbar-home");
+let mainTextHome = `
 
-// Lager og formaterer date objekt for meldinger i inboksen
-const months = [
-  'Jan',
-  'Feb',
-  'Mar',
-  'Apr',
-  'May',
-  'Jun',
-  'Jul',
-  'Augt',
-  'Sep',
-  'Oct',
-  'Nov',
-  'Dec'
-];
+<div class="home_page_container">
+  <div class="top_page_title_container">
+    <span>HOME</span>
+  </div>
 
-let today = new Date();
-const dd = String(today.getDate()).padStart(2, '0');
-const mIndex = today.getMonth();
-const mm = months[mIndex];
-const hours = String(today.getHours());
-const minutes = String(today.getMinutes()).padStart(2, '0');
-today = dd + " " + mm;
-const timeToday = hours + ":" + minutes;
-todayTimeTxt.innerHTML = timeToday;
-todayDateTxt.innerHTML = today;
+  <!-- Total sales container -->
+  <div class="total_sales_container">
+    <span class="total_sales_title">TOTAL SALES</span>
+    <span class="total_sales_past">PAST 7 DAYS</span>
+
+      <div class="week">
+        <span class="week_monday">M</span>
+        <span class="week_tuesday">T</span>
+        <span class="week_wednesday">W</span>
+        <span class="week_thursday">T</span>
+        <span class="week_friday">F</span>
+        <span class="week_saturday">S</span>
+        <span class="week_sunday">S</span>
+      </div>
+
+    <div class="diagrams_container">
+        <div class="diagram_1">
+           <div class="diagram_1_filled"></div>
+        </div>
+
+        <div class="diagram_2">
+          <div class="diagram_2_filled"></div>
+        </div>
+
+       <div class="diagram_3">
+          <div class="diagram_3_filled"></div>
+       </div>
+
+       <div class="diagram_4">
+          <div class="diagram_4_filled"></div>
+       </div>
+
+       <div class="diagram_5">
+          <div class="diagram_5_filled"></div>
+       </div>
+
+       <div class="diagram_6">
+          <div class="diagram_6_filled"></div>
+       </div>
+
+       <div class="diagram_7">
+        <div class="diagram_7_filled"></div>
+
+     </div>
+    </div>
+
+    <div class="sales_numbers_container">
+      <span>100K</span>
+      <span class="sales_75">75K</span>
+      <span class="sales_50">50K</span>
+      <span class="sales_25">25K</span>
+      <span class="sales_0">0</span>
+    </div>
+  </div><!-- Total sales container ends -->
+
+  <!-- Horisontal og vertical linje som deler grafene -->
+
+    <div class="horizontal_line"></div>
+    <div class="vertical_line"></div>
 
 
-// Simulerer gårsdagens dato som brukes på 2 av meldingene, og printer ut
-let yesterday = new Date();
-yesterday.setDate(yesterday.getDate() - 1);
-const ddYesterday = String(yesterday.getDate()).padStart(2, '0');
-const mmIndexYesterday = yesterday.getMonth();
-const mmYesterday = months[mmIndexYesterday];
-yesterday = ddYesterday + " " + mmYesterday;
-yesterdayTxt.forEach(day =>{
-  day.innerHTML = yesterday;
+
+  <!-- Budget -->
+
+  <div class="budget_page_container">
+  <span>BUDGET</span>
+    <div class="goal_circle">
+      <div class="goal_circle_inside">
+        <div class="goal_circle_inside2"></div>
+      </div>
+    </div>
+
+    <div class="total_circel">
+      <span class="accomplished_number">150 000</span>
+      <span class="total_goal_number">300 000</span>
+    </div>
+
+      <span class="percent_accomplished_txt">50% OF GOAL</span>
+  </div>
+
+  <!-- Profit -->
+  <div class="profit_container">
+
+      <span class="profit_title">PROFIT</span>
+
+
+      <div class="total_profit_div">
+      <span class="total_profit_number">$</span><span class="profit_k"> .-</span>
+      </div>
+  </div>
+</div>
+`
+// Loader opp homecontent når siden lastes opp og printer ut home content når brukeren klikker på Home
+window.onload = mainContentHome.innerHTML = mainTextHome;
+
+navbarHome.addEventListener("click", ()=>{
+  mainContentHome.innerHTML = mainTextHome;
+  loopNumber();
+  setTimeout(HomePageDiagramsModule.generateDiagramNumbers, 100);
 });
 
-// Function som slicer og printer message text til x antall bokstaver - Må jobbes mere med
-const msgArray = [
-   "Kenneth er syk idag. Kunne du sett om vi har noen som kan erstate kveldsvakta hans?",
-   "Kunne vi flyttet neste onsdags møte til torsdag? Jeg må være på restorangen fram til kl.12, men etter det er jeg ledig når som helst.",
-   "Hei. Prøvde å ringe. Kan du ringe meg så fort du ser dette?Takk"
-];
-function slicingMsgTxt(){
-  for (var i = 0; i < messageTxt.length; i++) {
-    let slicedTxt = msgArray[i].slice(0,30);
-    messageTxt[i].innerHTML = slicedTxt + "...";
-  }
-};
-window.onload = slicingMsgTxt();
+// Importerer Salesmodule for å bruke data inn i funksjonen under
+import SalesModule from '../Modules/SalesModule.js';
 
-// Toggler expanded class som åpner og stenger inbox meldinger
-// Toggler hidden class som gjemmer og viser tiden for meldingene
-// Printer ut hele meldingen og slicer den når meldingen ikke er ekspandert
+// Henter data fra SalesModule
+function totalNumber(){
+const salesArray = SalesModule.getAll();
+const totalProfit = salesArray[0].revenueAccThisYear + salesArray[1].revenueAccThisYear + salesArray[2].revenueAccThisYear + salesArray[3].revenueAccThisYear;
+return totalProfit;
+}
+// Lager array med data
+function makeNumbers(){
+    let numbers = [];
+    for(let i=31;i>0;i--){
+        numbers.push(totalNumber() / i)
+    }
+    return numbers;
+}
+// Printer ut tall i stigende rekkefølge
+function loopNumber(){
+    var counter = 0;
+    var numbers = makeNumbers();
+    var loopnumber = setInterval( changeNumbers, 50);
+    function changeNumbers() {
+        counter++;
+        if(counter === 30){
+            clearInterval(loopnumber)
+        }
+        document.querySelector(".total_profit_number").innerHTML= Math.floor(numbers[counter]);
+    }
+}
+loopNumber();
 
-expandable.forEach(element => {
-element.addEventListener("click", (e)=>{
-    e.currentTarget.children[0].classList.toggle("hidden");
-    e.currentTarget.classList.toggle("expanded");
-    e.currentTarget.children[1].children[0].classList.toggle("visible");
-    overlay.classList.toggle("overlay_active");
-  // Sjekker om brukeren har klikket på nyeste meldingen for å fjerne grønn border
-  if (e.currentTarget.children[1].children[2].children[0].innerHTML.length >50) {
-    slicingMsgTxt()
-  }else if(e.currentTarget.classList.contains("msg3_time")){
-    msg3.style.border="none";
-    e.currentTarget.children[1].children[2].children[0].innerHTML = msgArray[2]
-    e.currentTarget.children[1].children[2].children[0].style.display = "inline-block";
-  }else if (e.currentTarget.classList.contains("msg2_time")) {
-    e.currentTarget.children[1].children[2].children[0].innerHTML = msgArray[1];
-    e.currentTarget.children[1].children[2].children[0].style.display = "inline-block";
-  }else if (e.currentTarget.classList.contains("msg1_time")) {
-    e.currentTarget.children[1].children[2].children[0].innerHTML = msgArray[0];
-  }
+// Importerer HomePageDiagramsModule for å bruke funksjonen som henter data som fyller ut diagramen på home siden
+import HomePageDiagramsModule from '../modules/salesGenerator.js';
 
-  if(e.currentTarget.classList.contains("expanded")){
-    e.currentTarget.children[1].children[2].children[0].style.display="inline-block";
-  }else if (!e.currentTarget.classList.contains("expanded")) {
-    e.currentTarget.children[1].children[2].children[0].style.display="inline-block";
-  }
-
-});
-});
-
-// window.addEventListener("resize", function(){
-//     const msgTxtDiv = document.querySelector(".message_txt_div");
-//     if (document.documentElement.clientWidth < 920) {
-//       msgTxtDiv.style.display="none";
-//     }
-//     else {
-//       msgTxtDiv.style.display="inline-block";
-//     }
-// }, true);
-
-// Koden som henter username fra localStorage og printer det ut i inbox delen
-window.onload = inboxName.innerHTML = localStorage.getItem("username");
-
-
-// Viser inbox når bruker klikker på melding icone
-// Viser close icone for å stenge inbox
-const messageIcone = document.querySelector(".inbox-icone");
-const inboxContainer = document.querySelector(".inbox_container");
-const closeInbox = document.querySelector(".close-inbox");
-
-messageIcone.addEventListener("click", ()=>{
-  inboxContainer.style.display="inline-block";
-  messageIcone.style.display="none";
-  closeInbox.style.display="inline-block";
-})
-closeInbox.addEventListener("click", ()=>{
-  inboxContainer.style.display = "none";
-  messageIcone.style.display = "inline-block";
-  closeInbox.style.display = "none";
-})
-
-
-
-// Time function som printer ut melding etter vis antal sekunder
-function printMessage3(){
-  message3.style.visibility="visible";
-  var audio = new Audio("../Images/mail-notification.mp3");
-  audio.play();
-};
-window.onload = setTimeout(printMessage3, 6000);
-
-
-// Navbar funksjonalitet som setter border under iconer som indikerer hva brukeren har klikket på
-const home = document.querySelector(".navbar-home");
-const food = document.querySelector(".navbar-food");
-const employees = document.querySelector(".navbar-employees");
-const sales = document.querySelector(".navbar-sales");
-
-home.addEventListener("click", ()=>{
- home.style.borderBottom="4px solid #fff";
- food.style.borderBottom="none";
- employees.style.borderBottom="none";
- sales.style.borderBottom="none";
-});
-
-food.addEventListener("click", ()=>{
- home.style.borderBottom="none";
- food.style.borderBottom="4px solid #fff";
- employees.style.borderBottom="none";
- sales.style.borderBottom="none";
-});
-
-employees.addEventListener("click", ()=>{
- home.style.borderBottom="none";
- food.style.borderBottom="none";
- employees.style.borderBottom="4px solid #fff";
- sales.style.borderBottom="none";
-});
-
-sales.addEventListener("click", ()=>{
-
-
- home.style.borderBottom="none";
-
-
- food.style.borderBottom="none";
-
-
- employees.style.borderBottom="none";
-
-
- sales.style.borderBottom="4px solid #fff";
-});
+// setter timeout slik at transition funkerer på søylene
+window.onload =  setTimeout(HomePageDiagramsModule.generateDiagramNumbers, 100);
---
+++ modules/SalesModule.js
@@ -0,0 +1,131 @@
+const SalesModule = ( function (){
+
+    const salesArray = [
+    {
+        name: "Bjørvika",
+        revenueAccThisYear: 610000,
+        revenuePreviousYear: 990000,
+        revenuePreviousMonth: 190000,
+        revenuePreviousWeek: 35000,
+
+        revenuePreviousDay: 12000,
+        grossRevenueAccThisYear: 500000,
+        grossRevenuePreviousYear: 700000,
+
+        revenueDaysLastweek : [ 7000, 8500,9000,23500,11000,15500,26000]
+    },
+    {
+        name: "Nydalen",
+        revenueAccThisYear: 530000,
+        revenuePreviousYear: 800800,
+        revenuePreviousMonth: 150000,
+        revenuePreviousWeek: 45400,
+
+        revenuePreviousDay: 8000,
+        grossRevenueAccThisYear: 300000,
+        grossRevenuePreviousYear: 600000,
+        
+        revenueDaysLastweek : [ 5000, 9500,16000,10500,25000,15500,12000]
+    },
+    {
+        name: "Grunerløkka",
+        revenueAccThisYear: 710000,
+        revenuePreviousYear: 911000,
+        revenuePreviousMonth: 130000,
+        revenuePreviousWeek: 55500,
+
+        revenuePreviousDay: 14000,
+
+        grossRevenueAccThisYear: 500000,
+        grossRevenuePreviousYear: 900000,
+        
+        revenueDaysLastweek : [ 10000, 13500,15000,14500,16000,25500,16000]
+    },
+    {
+        name: "Karl-Johan",
+        revenueAccThisYear: 420000,
+        revenuePreviousYear: 754000,
+        revenuePreviousMonth: 125000,
+        revenuePreviousWeek: 42300,
+
+        revenuePreviousDay: 11000,
+
+        grossRevenueAccThisYear: 300000,
+        grossRevenuePreviousYear: 500000,
+        
+        revenueDaysLastweek : [ 20000, 23500,30000,4500,15000,35500,15000]
+    }
+    ]
+
+    const getAll = () => salesArray;
+        
+    /*funksjon for å hente ut totale-salg per dag for restaurantene */
+    const getTotalperDay = function (day){
+        let total = 0;
+        total += salesArray[0].revenueDaysLastweek[day];
+        total += salesArray[1].revenueDaysLastweek[day];
+        total += salesArray[2].revenueDaysLastweek[day];
+        total += salesArray[3].revenueDaysLastweek[day];
+      
+        total = (total/100000) * 100;
+        total = total +"%"
+        return total;
+    }
+
+    // kaller funksjonen med de ulike dagene.
+    const getTotalMonday = getTotalperDay(0);
+    const getTotalTuesday = getTotalperDay(1);
+    const getTotalWednesday = getTotalperDay(2);
+    const getTotalThursday = getTotalperDay(3);
+    const getTotalFriday = getTotalperDay(4);
+    const getTotalSaturday = getTotalperDay(5);
+    const getTotalSunday = getTotalperDay(6);
+
+
+
+    // funksjon som skal ta knappe-verdiene som parameter, (navn ,periode, category)
+
+    const getRevenueByRestaurantAndPeriodAndCategory = (name,period, category)  => {        
+        let restaurant = salesArray.filter( object => object.name=== name);
+        let revenueNumber = "";
+
+        if(period === "this-year"){
+             revenueNumber = restaurant[0].revenueAccThisYear;
+        }
+
+        else if(period === "last-year"){
+             revenueNumber = restaurant[0].revenuePreviousYear;
+        }
+
+        else if(period === "last-month"){
+            revenueNumber = restaurant[0].revenuePreviousMonth;
+       }
+
+        else if(period === "last-week"){
+            revenueNumber = restaurant[0].revenuePreviousWeek;
+        }
+
+         // tester for category og returner deretter ca profitt: pizza settes til 70%. Drikke til 20%. Salat til 10%.
+        
+        switch (category){
+            case "all":
+                return revenueNumber;
+            case "pizza":
+                return  Math.round(revenueNumber * 0.7);
+            case "drinks":
+                return Math.round(revenueNumber * 0.2);
+            case "salads":
+                return Math.round(revenueNumber  * 0.1);          
+        }
+     
+     };
+
+
+    
+    return {getAll, getTotalMonday,getTotalTuesday, getTotalWednesday,getTotalThursday,getTotalFriday,getTotalSaturday,getTotalSunday, getRevenueByRestaurantAndPeriodAndCategory};
+
+
+}())
+
+export default SalesModule
+
